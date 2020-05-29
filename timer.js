087b82e964abec2d5fe97c6971a64c68bd3aefcb
Heehyun8102 <k2h0395@gmail.com>
2020-05-30 01:05:36 +0900

    uncomplete timer

--- timer.js
+++ timer.js
@@ -1,10 +1,36 @@
+const starttbtn = document.querySelector('#startt');
+const resettbtn = document.querySelector('#resett');
+
+let m_input = document.getElementById('m_timer').value;
+let s_input = document.getElementById('s_timer').value;
+
+let timeAll = Number(m_input)*60 + Number(s_input);
+
+let timerFunction = function(){
+    
+    let minute = Math.floor(timeAll / 60);
+    let second = timeAll % 60;
+    timeAll -=1;
 
 
-var temp;
-const startbtn = document.querySelector('#startt');
+    if(minute<10) minute = '0'+ minute;
+    if(second<10) second = '0'+ second;
+    let timeshow = minute + ':' + second;
 
-startbtn.onclick = function(){
-var minute = document.getElementById('m_timer').value;
-temp = minute;
+    document.querySelector('#w_timer h1').textContent = timeshow;
+}
 
-document.querySelector('#w_timer h1').textContent = temp;}
+let b;
+starttbtn.onclick = function(){
+    if (starttbtn.textContent === 'start'){
+        starttbtn.textContent = 'stop';
+        b = setInterval(timerFunction, 1000);
+    }else{
+        starttbtn.textContent = 'start';
+        clearInterval(b);
+    }
+}
+
+resettbtn.onclick = function(){
+    document.querySelector('#w_timer h1').textContent = '00:00';
+}
